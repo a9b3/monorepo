220439ff8345dfed3884edaa9040b49d058ff695
Sam Lau <esayemm@gmail.com>
2022-04-11 00:12:50 -1000

    update devx scripts

--- devx/scripts/zsh_completions.sh
+++ devx/scripts/zsh_completions.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+set -eu
+
+# IMPORTANT:
+# make sure your fpath includes ~/.zsh/completion you can do this in your zshrc
+# have the following line before compinit
+#     fpath = (~/.zsh/completion $fpath)
+#
+mkdir -p ~/.zsh/completion
 minikube completion zsh > ~/.zsh/completion/_minikube
 kubectl completion zsh > ~/.zsh/completion/_kubectl
