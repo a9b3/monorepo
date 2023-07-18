fe100e8c2cfadf035c51aee30ad7867e2f4ead20
Sam Lau <esayemm@gmail.com>
2023-07-17 14:43:24 -1000

    add daw-core daw-fe

---
+++ repos/daw-fe/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import hi from "@monorepo/daw-core"
+  hi()
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
