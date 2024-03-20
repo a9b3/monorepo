3dec7d0a339645d6c6588f14b94967bf6ee62d4c
Sam Lau <esayemm@gmail.com>
2024-03-20 15:02:24 -0700

    chore(root): formatting

--- flake.nix
+++ flake.nix
@@ -18,80 +18,83 @@
         let pkgs = nixpkgs.legacyPackages.${system}; in
         {
           devShell = pkgs.mkShell {
-            buildInputs = let
-              sqlc = with pkgs; stdenv.mkDerivation rec {
-                name = "sqlc";
-                # Need to provide a custom builder since the default assumes there's a
-                # makefile in the source
-                builder = pkgs.writeText "builder.sh" ''
-                  source $stdenv/setup
-                  mkdir -p $out/bin
-                  cp $src/sqlc $out/bin/sqlc
-                  chmod +x $out/bin/sqlc
-                '';
-                src = fetchzip {
-                  url = "https://github.com/kyleconroy/sqlc/releases/download/v1.13.0/sqlc_1.13.0_darwin_amd64.zip";
-                  sha256 = "sha256-izOrdNg8ZxXMwSEA0dv5IKOap+POwsM94+Eu9T1Xco4=";
+            buildInputs =
+              let
+                sqlc = with pkgs; stdenv.mkDerivation {
+                  name = "sqlc";
+                  # Need to provide a custom builder since the default assumes there's a
+                  # makefile in the source
+                  builder = pkgs.writeText "builder.sh" ''
+                    source $stdenv/setup
+                    mkdir -p $out/bin
+                    cp $src/sqlc $out/bin/sqlc
+                    chmod +x $out/bin/sqlc
+                  '';
+                  src = fetchzip {
+                    url = "https://github.com/kyleconroy/sqlc/releases/download/v1.13.0/sqlc_1.13.0_darwin_amd64.zip";
+                    sha256 = "sha256-izOrdNg8ZxXMwSEA0dv5IKOap+POwsM94+Eu9T1Xco4=";
+                  };
                 };
-              };
-              terraform = with pkgs; stdenv.mkDerivation rec {
-                name = "terraform";
-                # Need to provide a custom builder since the default assumes there's a
-                # makefile in the source
-                builder = pkgs.writeText "builder.sh" ''
-                  source $stdenv/setup
-                  mkdir -p $out/bin
-                  cp $src/terraform $out/bin/terraform
-                  chmod +x $out/bin/terraform
-                '';
-                src = fetchzip {
-                  url = "https://releases.hashicorp.com/terraform/1.3.2/terraform_1.3.2_darwin_arm64.zip";
-                  sha256 = "sha256-KzKA/ORUui/0GHvTO2ox0EV6MM9QWV9n1Lw5OdgP4C4=";
+                terraform = with pkgs; stdenv.mkDerivation {
+                  name = "terraform";
+                  # Need to provide a custom builder since the default assumes there's a
+                  # makefile in the source
+                  builder = pkgs.writeText "builder.sh" ''
+                    source $stdenv/setup
+                    mkdir -p $out/bin
+                    cp $src/terraform $out/bin/terraform
+                    chmod +x $out/bin/terraform
+                  '';
+                  src = fetchzip {
+                    url = "https://releases.hashicorp.com/terraform/1.3.2/terraform_1.3.2_darwin_arm64.zip";
+                    sha256 = "sha256-KzKA/ORUui/0GHvTO2ox0EV6MM9QWV9n1Lw5OdgP4C4=";
+                  };
                 };
-              };
-            in
-            [
-              # A wrapper around bazel that will invoke the version specified in
-              # the .bazelversion file in your project directory.
-              # https://github.com/bazelbuild/bazelisk
-              pkgs.bazelisk
-              pkgs.bazel-buildtools
+              in
+              [
+                # A wrapper around bazel that will invoke the version specified in
+                # the .bazelversion file in your project directory.
+                # https://github.com/bazelbuild/bazelisk
+                pkgs.bazelisk
+                pkgs.bazel-buildtools
 
-              # cli
-              pkgs.gnupg
-              pkgs.awscli2
-              terraform
-              pkgs.terragrunt
-              pkgs.kubectx
-              pkgs.grpcurl
-              pkgs.fd
-              pkgs.sd
+                # cli
+                pkgs.gnupg
+                pkgs.awscli2
+                terraform
+                pkgs.terragrunt
+                pkgs.kubectx
+                pkgs.grpcurl
+                pkgs.fd
+                pkgs.sd
+                pkgs.tree
 
-              # A tool for exploring each layer in a docker image
-              pkgs.dive
+                # A tool for exploring each layer in a docker image
+                pkgs.dive
 
-              # linters
-              pkgs.sqlfluff
-              pkgs.yamllint
+                # linters
+                pkgs.sqlfluff
+                pkgs.yamllint
 
-              # go tools
-              pkgs.go-migrate
-              sqlc
+                # go tools
+                pkgs.gopls
+                pkgs.go-migrate
+                sqlc
 
-              # source management
-              pkgs.pre-commit
+                # source management
+                pkgs.pre-commit
 
-              pkgs.skaffold
-              pkgs.kubectl
-              pkgs.minikube
+                pkgs.skaffold
+                pkgs.kubectl
+                pkgs.minikube
 
-              # ---------------------------------------------------------------
-              # Lang Support
-              # ---------------------------------------------------------------
-              pkgs.go
-              pkgs.nodejs_20
-              pkgs.nodejs_20.pkgs.pnpm
-            ];
+                # ---------------------------------------------------------------
+                # Lang Support
+                # ---------------------------------------------------------------
+                pkgs.go
+                pkgs.nodejs_20
+                pkgs.nodejs_20.pkgs.pnpm
+              ];
             shellHook = ''
               echo "----------------------------------------------------------"
               echo "Run the following to setup dev environment."
--- go.mod
+++ go.mod
@@ -19,10 +19,12 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/Code-Hex/go-wordwrap v1.0.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -49,11 +51,21 @@ require (
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	go.uber.org/atomic v1.6.0 // indirect
-	golang.org/x/crypto v0.0.0-20211215165025-cf75a172585e // indirect
-	golang.org/x/net v0.0.0-20211013171255-e13a2654a71e // indirect
-	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20221212164502-fae10dda9338 // indirect
+	golang.org/x/mod v0.15.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/telemetry v0.0.0-20240209200032-7b892fcb8a78 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.18.1-0.20240311201521-78fbdeb61842 // indirect
+	golang.org/x/tools/gopls v0.15.2 // indirect
+	golang.org/x/vuln v1.0.1 // indirect
 	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	honnef.co/go/tools v0.4.6 // indirect
+	mvdan.cc/gofumpt v0.6.0 // indirect
+	mvdan.cc/xurls/v2 v2.5.0 // indirect
 )
